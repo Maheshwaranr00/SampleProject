a7633dfd93f7a02384e1f475b801e9433aa77937
Maheshwaranr00 <143581406+Maheshwaranr00@users.noreply.github.com>
2023-08-30 13:12:22 +0530

    Update demo-todo-app.spec.ts

--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -4,10 +4,7 @@ test.beforeEach(async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc');
 });
 
-test.describe('New Todo', () => {
-  test('should allow me to add todo items', async ({ page }) => {
-    // create a new todo locator
-    const newTodo = page.getByPlaceholder('What needs to be done?');
+
 
     // Create 1st todo.
     await newTodo.fill(TODO_ITEMS[0]);
